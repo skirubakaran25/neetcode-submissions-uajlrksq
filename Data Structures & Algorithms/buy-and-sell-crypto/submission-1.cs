0ec68e3c26c969a709cdadf5d810dc597758d096
Kirubakaran S <skirubakaran25@gmail.com>
2026-04-05 13:37:38 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+
+        int minPrice = int.MaxValue;
+        int maxProfit = 0;
+
+        foreach(int price in prices)
+        {
+            if(price < minPrice)
+            {
+                minPrice = price;
+            }
+            else
+            {
+                int profit = price - minPrice;
+                maxProfit = Math.Max(maxProfit, profit);
+            }
+        }
+        return maxProfit;       
+    }
+}
