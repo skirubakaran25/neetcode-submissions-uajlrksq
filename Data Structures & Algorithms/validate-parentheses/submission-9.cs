191798c4a965175ca3b90d458513623ea9b5f9d8
Kirubakaran S <skirubakaran25@gmail.com>
2026-04-23 11:48:39 +0530

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public bool IsValid(string s) {
+
+        Stack<char> stack = new Stack<char>();
+
+
+        foreach(char item in s)
+        {
+            if(item == '{' || item == '(' || item == '[' )
+            {
+                stack.Push(item);
+            }
+            else
+            {
+                if(stack.Count == 0)
+                {
+                    return false;
+                }
+                char popped = stack.Pop();
+                if((popped == '[' && item != ']') || (popped == '{' && item != '}') || (popped == '(' && item != ')'))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return (stack.Count == 0);
+    }
+}
