944a32dac918c15fc0128b093c71d73448272190
Kirubakaran S <skirubakaran25@gmail.com>
2026-05-24 20:49:02 +0530

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.Length - 1;
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if (target == nums[mid])
+                return mid;
+            else if (target > nums[mid])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+}
